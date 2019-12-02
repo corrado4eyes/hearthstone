01bee965e2ef889b9da1d3fdc7e4dca025248a84
corrado4eyes <corrado@splendo.com>
2019-12-02 15:45:46 +0100

    [#4] Updated App to inject container

--- hearthstone/src/App.tsx
+++ hearthstone/src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import "reflect-metadata";
+import ServiceFactory from './services/serviceFactory';
+import * as Inversify from "inversify-react";
+import container from '../config/inversify';
+
+const serviceFactory = new ServiceFactory(container);
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-    </div>
+    <Inversify.Provider container={container}>
+        <div className="App">
+          {getData()}
+        </div>
+    </Inversify.Provider>
   );
+}
+
+const getData = () => {
+  return serviceFactory.getCardService().getAll()
+  .then((value: any) => console.log(value))
+  .catch((error: any) => console.log("dio cane"))
 }
 
 export default App;
