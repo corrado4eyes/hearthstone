c94f949794789c9e360c389b7743c13f2693188e
corrado4eyes <corrado@splendo.com>
2019-12-03 14:47:33 +0100

    [#1] Added test for app.tsx

--- hearthstone/__tests__/App.test.tsx
+++ hearthstone/__tests__/App.test.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../src/App';
+import {shallow, ShallowWrapper} from 'enzyme'
+import { dummyCardArray } from './__mocks__/mockObjects';
+import Card from '../src/model/card';
+
 
 describe('App', () => {
-    it(('renders n elements in an array'))
+    const cards: Card[] = dummyCardArray
+    it('renders n elements in an array', () => {
+        const component: ShallowWrapper = shallow(<App/>);
+        component.setState({cards: cards})
+        const divs: ShallowWrapper = component.find('.card');
+        expect(divs).toHaveLength(cards.length);
+    });
+
+    it('Snapshot testing', () => {
+            const component: ShallowWrapper = shallow(<App/>);
+            component.setState({cards: cards})
+            expect(component).toMatchSnapshot();
+    });
 });
