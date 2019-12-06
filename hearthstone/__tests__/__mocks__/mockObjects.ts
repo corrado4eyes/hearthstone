6a9e4428e270316f08f3d998960f47c686f411ad
corrado4eyes <corrado@splendo.com>
2019-12-06 17:16:27 +0100

    [#15] Added dummy variable to mockObj

--- hearthstone/__tests__/__mocks__/mockObjects.ts
+++ hearthstone/__tests__/__mocks__/mockObjects.ts
@@ -20,6 +20,26 @@ export const dummyCard: Card = {
     type: "Developer"
 }
 
-export const dummyCardArray = [dummyCard]
+export const dummyCardUgly: Card = {
+    artist: "lala",
+    cardId: "what an ugly dummy card!",
+    cardSet: "Not Dummy",
+    collectible: true,
+    cost: 9999,
+    faction: "Splendo",
+    flavor: "Meat",
+    howToGet: "Ask Frits",
+    howToGetGold: "Feeds and Walk Frits for 2 years every day (excluded the holidays)",
+    img: "http://media.services.zam.com/v1/media/byName/hs/cards/enus/DS1_183.png",
+    imgGold: "http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/DS1_183_premium.gif",
+    locale: "enUS",
+    name: "corrado4eyes",
+    playerClass: "Human",
+    rarity: "Not Super Rare",
+    text: "A dummy Italian boy",
+    type: "Developer"
+}
+
+export const dummyCardArray = [dummyCard, dummyCardUgly]
 
 export const cardNotFoundError = "Cards not Found!";
