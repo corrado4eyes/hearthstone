7698f6581c83239abbcab3b12017aed265cb25da
corrado4eyes <corrado@splendo.com>
2019-12-06 17:30:42 +0100

    [#15] Added test case for new actions

--- hearthstone/__tests__/redux/reducers/cardReduce.test.ts
+++ hearthstone/__tests__/redux/reducers/cardReduce.test.ts
@@ -2,16 +2,16 @@ import * as fromReducer from '../../../src/redux/reducers/cardReduces';
 import * as fromAction from '../../../src/redux/actions/cardActions';
 
 import { CardActionsType } from '../../../src/redux/actions/cardActions';
-import { dummyCardArray, cardNotFoundError } from '../../__mocks__/mockObjects';
+import { dummyCardArray, cardNotFoundError, dummyCard, dummyCardUgly } from '../../__mocks__/mockObjects';
 
 describe('Card Reducer', () => {
     let payload: CardActionsType;
     let state: fromReducer.State;
-    let mockState: fromReducer.State = {...fromReducer.initialState};
+    let mockState: fromReducer.State;
 
     beforeEach(() => {
         payload = fromAction.onSyncCardsConstructor();
-        mockState = {...fromReducer.initialState};
+        mockState = {...fromReducer.initialState, cards: dummyCardArray};
         state = fromReducer.reducer(mockState, payload);
     });
 
@@ -29,5 +29,32 @@ describe('Card Reducer', () => {
         state = fromReducer.reducer(mockState, payload);
         expect(state.loading).toBeFalsy()
         expect(state.error).toBe(cardNotFoundError)
+    });
+
+    const scenario = [
+        {
+            key: "rarity",
+            value: "Super Rare",
+            expectedValue: dummyCard
+        },
+        {
+            key: "cardSet",
+            value: "Not Dummy",
+            expectedValue: dummyCardUgly
+        }
+    ]
+    scenario.forEach((el: any) => {
+        it(`filters by ${el.key}`, () => {
+            payload = fromAction.onSubmitFilterConstructor(el.value, el.key)
+            state = fromReducer.reducer(mockState, payload);
+            expect(state.filteredCards).toStrictEqual([el.expectedValue])
+        });
+    });
+
+    it(`filters by name`, () => {
+        const cardName = "corrado"
+        payload = fromAction.onFilterByNameConstructor(cardName);
+        state = fromReducer.reducer(mockState, payload);
+        expect(state.filteredCards).toStrictEqual([dummyCard])
     });
 });
