cb13d8619b2bf295bbc0be0402bad6a78f466a5c
corrado4eyes <corrado@splendo.com>
2019-12-06 08:31:10 +0100

    [#13] Added Enum for filterBar

---
+++ hearthstone/src/model/cardSet.ts
@@ -0,0 +1,19 @@
+export enum CardSet {
+    Basic = "Basic",
+    Classic = "Classic",
+    HallOfFame = "Hall of Fame",
+    Naxxramas = "Naxxramas",
+    Goblins_vs_Gnomes = "Goblins vs Gnomes",
+    Blackrock_Mountain = "Blackrock Mountain",
+    The_Grand_Tournament = "The Grand Tournament",
+    The_League_of_Explorers = "The League of Explorers",
+    Whispers_of_the_Old_Gods = "Whispers of the Old Gods",
+    One_Night_in_Karazhan = "One Night in Karazhan",
+    Mean_Streets_of_Gadgetzan = "Mean Streets of Gadgetzan",
+    Journey_to_UnGoro = "Journey to Un'Goro",
+    Tavern_Brawl = "Tavern Brawl",
+    Hero_Skins = "Hero Skins",
+    Missions = "Missions",
+    Credits = "Credits",
+    Debug = "Debug"
+}
