af4ac23568ab5ef837df59459b27d95ede3582cd
corrado4eyes <corrado@splendo.com>
2019-12-09 11:12:13 +0100

    [#19] Added utils file

---
+++ hearthstone/src/utils/utils.ts
@@ -0,0 +1,10 @@
+export const urlIsFound = (url: string) => {
+    return fetch(url)
+    .then((response) => {
+        if (response.status === 404) {
+            return false;
+        } else {
+            return true;
+        }
+    }).catch((error) => error);
+}
