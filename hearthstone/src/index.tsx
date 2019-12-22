76617300350a86bc5db142d23f5d9db8c48fd79a
corrado4eyes <corrado@splendo.com>
2019-12-22 18:45:10 +0100

    [#3] Added firebase config to index.tsx

--- hearthstone/src/index.tsx
+++ hearthstone/src/index.tsx
@@ -7,7 +7,11 @@ import * as Inversify from "inversify-react";
 import container from '../config/inversify';
 import { configureStore } from './redux/store/storeConfig';
 import * as Redux from "react-redux";
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import config from '../config/firebaseConfig'
 
+firebase.initializeApp(config);
 const store = configureStore(container);
 ReactDOM.render(
     <Inversify.Provider container={container}>
