79c0b3b8aa1c98a4b12c6805e5267d12c003eb7f
corrado4eyes <corrado@splendo.com>
2019-12-22 21:39:06 +0100

    [#3] Added config file

---
+++ hearthstone/config/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDvnIylbJ5cHXcKj1UxOie7XzwnLYujFW8",
+    authDomain: "hearthstone-64296.firebaseapp.com",
+    databaseURL: "https://hearthstone-64296.firebaseio.com",
+    projectId: "hearthstone-64296",
+    storageBucket: "hearthstone-64296.appspot.com",
+    messagingSenderId: "1074488811093",
+    appId: "1:1074488811093:web:fad6a85d8e8a431a9dfedc",
+    measurementId: "G-YVPJTXLNYE"
+}
