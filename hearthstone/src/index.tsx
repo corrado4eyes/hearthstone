417af607820512f9198bcea2d3d18e37274a9e40
corrado4eyes <corrado@splendo.com>
2019-12-23 15:23:35 +0100

    [#3] Updated index

--- hearthstone/src/index.tsx
+++ hearthstone/src/index.tsx
@@ -10,13 +10,14 @@ import * as Redux from "react-redux";
 import * as firebase from "firebase/app";
 import "firebase/firestore";
 import config from '../config/firebaseConfig'
+import { CardSet } from './model/cardSet';
 
 firebase.initializeApp(config);
 const store = configureStore(container);
 ReactDOM.render(
     <Inversify.Provider container={container}>
         <Redux.Provider store={store}>
-            <App cardSet={"Basic"}/>
+            <App />
         </Redux.Provider>
     </Inversify.Provider>
 , document.getElementById('root'));
