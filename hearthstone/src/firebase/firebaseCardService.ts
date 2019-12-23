b14c01b11dfde0111818901468774ffdcf2c37aa
corrado4eyes <corrado@splendo.com>
2019-12-23 15:24:53 +0100

    [#3] Added firestore implementation

--- hearthstone/src/firebase/firebaseCardService.ts
+++ hearthstone/src/firebase/firebaseCardService.ts
@@ -4,21 +4,49 @@ import * as firebase from "firebase/app";
 import { injectable, inject } from 'inversify';
 import types from './types';
 import { IFirebaseCommon } from './firebaseCommon';
+import { CardSet } from '../model/cardSet';
 
 @injectable()
 export class FirebaseCardService implements CardService {
     constructor(@inject(types.FirebaseCommon) private readonly firebaseCommon: () => IFirebaseCommon){}
 
-    save(card: Card): any {
-        const docRef = this.firebaseCommon().collection('cards')
-        return docRef.doc(card.cardId!)
-        .set(card)
+    docRef = (): firebase.firestore.CollectionReference => {
+        return this.firebaseCommon().collection('cards')
+    }
+
+    save(card: Card): Promise<boolean> {
+        return this.docRef().doc(card.cardId!).set(card)
+        .then(() => true)
+        .catch((err) => err);
     }    
     get(cardId: string): Promise<Card> {
-        throw new Error("Method not implemented.");
+        return this.docRef().doc(cardId).get()
+        .then((doc: firebase.firestore.DocumentData) => doc.data())
+        .catch((err) => err);
     }
     getAll(): Promise<Card[]> {
-        throw new Error("Method not implemented.");
+        return this.docRef().get()
+        .then((doc: firebase.firestore.QuerySnapshot) => {
+            return doc.docs.map((el: firebase.firestore.DocumentData) => {
+                return el.data()
+            });
+        })
+        .catch((err) => {
+            return err
+        });
+    }
+
+    getByCardSet(cardSet: CardSet): Promise<Card[]> {
+        const filter = {'field': 'cardSet', 'operator': "==", 'cardSet': cardSet}
+        return this.docRef().limit(10).where(filter.field, '==', filter.cardSet).get()
+        .then((doc: firebase.firestore.QuerySnapshot) => {
+            return doc.docs.map((el: firebase.firestore.DocumentData) => {
+                return el.data()
+            });
+        })
+        .catch((err) => {
+            return err
+        });
     }
 
 
