70b40c239c6999bdf78ce80414a1709644cc0484
corrado4eyes <corradoquattrocchi@hotmail.it>
2020-01-02 08:41:24 +0100

    [#24] Created test for cardDetail

---
+++ hearthstone/__tests__/uiComponents/cardDetail.test.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import {CardDetail} from '../../src/uiComponents/cardDetail';
+import { shallow, ShallowWrapper, ReactWrapper, mount } from 'enzyme';
+import { dummyCard } from '../__mocks__/mockObjects';
+import { CardState } from '../../src/uiComponents/cardComponent';
+import { CardDetailProps } from '../../src/uiComponents/cardDetail';
+import { Button } from 'react-bootstrap';
+
+const addToFavorite = jest.fn()
+const switchImage = jest.fn()
+
+const cardStateInitialValues: CardState = {
+    goldImg: false,
+    isImgAvailable: true,
+    isOpen: false
+}
+
+
+describe('CardDetail', () => {
+    const component: ReactWrapper = mount(<CardDetail switchImage={switchImage} addToFavourites={addToFavorite} cardState={cardStateInitialValues} card={dummyCard}/>);
+
+    it('matches the snapshot', () => {
+        expect(component).toMatchSnapshot();
+    })
+
+    it('checks the props', () => {
+        const props: CardDetailProps = (component.find(CardDetail).props() as CardDetailProps)
+        expect(props.cardState.goldImg).toBe(cardStateInitialValues.goldImg)
+        expect(props.cardState.isImgAvailable).toBe(cardStateInitialValues.isImgAvailable)
+        expect(props.cardState.isOpen).toBe(cardStateInitialValues.isOpen)
+    })
+
+    it('fires swithImage onClick', () => {
+        const switchButton = component.find('#switchImg').find(Button)
+        expect(switchImage).not.toHaveBeenCalled()
+        switchButton.simulate('click')
+        expect(switchImage).toHaveBeenCalled()
+    });
+
+    it('fires addToFavorite onClick', () => {
+        const switchButton = component.find('#favBtn').find(Button)
+        expect(addToFavorite).not.toHaveBeenCalled()
+        switchButton.simulate('click')
+        expect(addToFavorite).toHaveBeenCalled()
+    });
+});
